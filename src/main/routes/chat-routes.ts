a5d66b0e2ffa68460526ca4236ecf924f970f702
Eduardo Santana <eduardosantana20122002@gmail.com>
2023-06-12 17:08:03 -0300

    Update chat-routes.ts

--- src/main/routes/chat-routes.ts
+++ src/main/routes/chat-routes.ts
@@ -5,7 +5,7 @@ import { makeLoadRecentMessagesController } from "../factories/controllers/chat/
 import { makeLoadRoomController } from "../factories/controllers/chat/loadRoom";
 import {Router} from 'express';
 
-export default router = Router();
+export default const router = Router();
 const authAdapter = new UserAdapter();
 
 router.get("/chat/room/:userId", authAdapter.make(makeLoadRoomController()));
