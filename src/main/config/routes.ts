87badeb9c721a6c89d366194122b47d54b28109f
Eduardo Santana <eduardosantana20122002@gmail.com>
2023-06-12 13:16:39 -0300

    Update routes.ts

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,6 +5,10 @@ import userRouter from '../routes/user-routes'
 import projectRouter from '../routes/project-routes'
 
 export default async(app: Express) =>{
+  app.get("/ping", (req, res)=> {
+      console.log("working");
+    return res.status(200).json({ola: 'ola' });
+  });
   app.use("/api", chatRouter);
   app.use("/api", postRouter);
   app.use("/api", userRouter);
