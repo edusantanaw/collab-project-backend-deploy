59c4f67193dc11fc22cbbfe5966182fb1fdb2d18
Eduardo Santana <eduardosantana20122002@gmail.com>
2023-06-12 13:22:47 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 import { Server } from "./src/main/server";
+import prisma from '../infra/prisma';
 
 const server  = new Server();
+
+
+process.on("exit", ()=> {
+    prisma.$disconnect(); 
+})
 
 server.bootstrap();
