1daf076c53643f5b4e28777893de7c58734abcf8
Eduardo Santana <eduardosantana20122002@gmail.com>
2023-06-12 10:17:08 -0300

    Update routes.ts

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,12 @@
 import { Express, Router } from "express"
-import fg from 'fast-glob'
+import chatRouter from '../routes/chat-routes'
+import postRouter from '../routes/post-routes'
+import userRouter from '../routes/user-routes'
+import projectRouter from '../routes/project-routes'
 
 export default async(app: Express) =>{
-  const router = Router();
-  app.use("/api", router);
-  fg.sync("**/src/main/routes/**routes.ts").forEach(async (file: any) =>
-    (await import(`../../../${file}`)).default(router)
-  );
+  app.use(/"api", chatRouter);
+  app.use(/"api", postRouter);
+  app.use(/"api", userRouter);
+  app.use(/"api", projectRouter);
 }
